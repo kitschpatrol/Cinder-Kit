ea9ca2b1a62d566d299c07bea587f5d175138575
Eric Mika <eric@ericmika.com>
2016-01-17 02:01:40 -0500

    Number formatting.

--- src/CinderKit.h
+++ src/CinderKit.h
@@ -47,5 +47,27 @@ static std::string getUUIDString() {
 std::string toCodeString(ci::vec2 value) {
 	return "ci::vec2(" + ci::toString(value.x) + " ," + ci::toString(value.y) + ")";
 }
+
+// From Reza Ali's Cinder UI
+template <typename T>
+std::string toString(T value, int precision, int width, char fill) {
+	std::ostringstream out;
+	out << std::fixed << std::setfill(fill) << std::setw(width) << std::setprecision(precision) << value;
+	return out.str();
+}
+
+template <typename T>
+std::string toString(T value, int precision, char fill) {
+	std::ostringstream out;
+	out << std::fixed << std::setfill(fill) << std::setprecision(precision) << value;
+	return out.str();
+}
+
+template <typename T>
+std::string toString(T value, int precision) {
+	std::ostringstream out;
+	out << std::fixed << std::setfill('0') << std::setprecision(precision) << value;
+	return out.str();
+}
 }
 } // namespace kp::kit
