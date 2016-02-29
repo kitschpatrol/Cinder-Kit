5b197312902ba5fb6b8d89b0ff7c879a0351d1c8
Eric Mika <eric@ericmika.com>
2016-02-28 19:49:50 -0800

    NanoVG to Texture convenience lambda wrapper.

---
+++ src/NanoTexture.cpp
@@ -0,0 +1,52 @@
+// NanoTexture.cpp
+// RenderToTexture
+//
+// Created by Eric Mika on 2/28/16.
+//
+
+#include "cinder/Log.h"
+
+#include "NanoTexture.h"
+
+NanoTextureRef NanoTexture::create() {
+	return NanoTexture::create(512, 512);
+}
+
+NanoTextureRef NanoTexture::create(float width, float height) {
+	NanoTextureRef ref = std::shared_ptr<NanoTexture>(new NanoTexture());
+	ref->setup(width, height);
+	return ref;
+}
+
+NanoTexture::NanoTexture() {
+	CI_LOG_V("NanoTexture Created");
+}
+
+NanoTexture::~NanoTexture() {
+	CI_LOG_V("NanoTexture Destroyed");
+}
+
+void NanoTexture::setup(float width, float height) {
+	// auto fboSize = mBounds.getSize() * getWindow()->getContentScale(); // TODO HIDPI
+	mFbo = ci::gl::Fbo::create(width, height, ci::gl::Fbo::Format().stencilBuffer());
+	mCtx = std::make_shared<ci::nvg::Context>(ci::nvg::createContextGL()); // todo unique?
+}
+
+void NanoTexture::renderWithFunction(const std::function<void(ci::nvg::Context &, float, float)> &renderFunction) {
+	auto &vg = *mCtx;
+
+	ci::gl::ScopedFramebuffer fboScope(mFbo);
+	ci::gl::viewport(mFbo->getSize());
+	ci::gl::clear(ci::ColorAf::zero());
+	ci::gl::clear(GL_STENCIL_BUFFER_BIT);
+
+	vg.beginFrame(mFbo->getSize(), 1.0); // TODO HIDPI
+	renderFunction(vg, mFbo->getWidth(), mFbo->getHeight());
+	vg.endFrame();
+
+	mTexture = mFbo->getColorTexture();
+}
+
+const ci::gl::TextureRef NanoTexture::getTexture() const {
+	return mTexture;
+}
